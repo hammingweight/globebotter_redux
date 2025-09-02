77273cb6d8b2de25df43ca093a41ab99bf436dec
Carl Meijer <hammingweight@gmail.com>
2025-09-02 11:18:44 +0200

    Add history.

--- src/globebotter/rag.py
+++ src/globebotter/rag.py
@@ -1,6 +1,11 @@
-from typing import Annotated
+from typing import Annotated, List
+from typing_extensions import TypedDict
 
+from langchain_core.documents import Document
+from langchain_core.messages import AIMessage, HumanMessage
 from langchain_core.prompts import ChatPromptTemplate
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import add_messages, END, START, StateGraph
 
 from .llm import chat_model
 from .retriever import DocumentRetriever
@@ -16,11 +21,54 @@ system_prompt = (
 )
 
 retriever = DocumentRetriever()
-prompts = ChatPromptTemplate.from_messages(
+prompt = ChatPromptTemplate.from_messages(
     [
         ("system", system_prompt),
+        ("placeholder"), "{history}",
         ("human", "{question}"),
     ]
 )
 
-print(chat_model)
+class State(TypedDict):
+    question: str
+    context: List[Document]
+    answer: str
+    messages: Annotated[list, add_messages]
+
+
+def retrieve(state: State):
+    retrieved_docs = retriever.invoke(state["messages"][-1].content)
+    return {"context": retrieved_docs}
+
+def generate(state: State):
+    docs_content = "\n\n".join(doc.page_content for doc in state["context"])
+    messages = prompt.invoke(
+        {"question": state["messages"][-1].content, "history": state["messages"][:-1], "context": docs_content}
+    )
+    response = chat_model.invoke(messages)
+    return {"answer": response.content, "messages": AIMessage(response.content)}
+
+graph_builder = StateGraph(State).add_sequence(
+    [retrieve, generate]
+)
+graph_builder.add_edge(START, "retrieve")
+graph_builder.add_edge("generate", END)
+memory = MemorySaver()
+graph = graph_builder.compile(checkpointer=memory)
+config = {"configurable": {"thread_id": "abc123"}}
+
+input_messages = [HumanMessage("Suggest three sites that I should visit if I spend one day in Rome.")]
+response = graph.invoke({"messages": input_messages}, config=config)
+print(response["answer"])
+print("==================================================================\n\n")
+
+input_messages = [HumanMessage("Can you suggest a fourth site?")]
+response = graph.invoke({"messages": input_messages}, config=config)
+print(response["answer"])
+print("==================================================================\n\n")
+
+input_messages = [HumanMessage("Can you suggest a restaurant for lunch?")]
+response = graph.invoke({"messages": input_messages}, config=config)
+print(response["answer"])
+
+
