7434d4e59f1f4ddb676cefa146788e0876569c82
Carl Meijer <hammingweight@gmail.com>
2025-09-15 06:05:07 +0200

    Add image.

--- streamlit/app.py
+++ streamlit/app.py
@@ -6,7 +6,6 @@ from globebotter.rag import chatbot
 
 st.set_page_config(page_title="GlobeBotter Redux", page_icon="🇮🇹")
 st.markdown("<h2 style='text-align: center;'>Welcome to GlobeBotter Redux!</h2>", unsafe_allow_html=True)
-# st.header("Welcome to GlobeBotter Redux!")
 st.markdown(
     "<center><h4>An AI Chatbot for Italian Travel 🇮🇹</h4></center>", unsafe_allow_html=True
 )
@@ -15,9 +14,9 @@ __This chatbot was inspired by Valentina Alto's fun
 [GlobeBotter](https://github.com/PacktPublishing/Building-LLM-Powered-Applications/blob/main/Chapter%206%20-%20Building%20conversational%20apps.ipynb)
 LLM application.__
 
-Valentina Alto's chatbot is a LangChain chatbot that incorporates RAG and tools (Google search).
+Valentina Alto's chatbot is a LangChain application that incorporates RAG and tools (Google search).
 In particular, questions about Italy retrieve information from an Italian tourism guide stored in
-a FAISS vector database. The LLM used is an OpenAI model. *GlobeBotter* was written before the
+a FAISS vector database. The LLM is an OpenAI model. *GlobeBotter* was written before the
 release of LangGraph.
 
 *GlobeBotter Redux* uses LangGraph to build a RAG chatbot. It runs on a local LLM (Qwen3-4B) and
@@ -25,7 +24,7 @@ uses ChromaDB. This application is more limited than *GlobeBotter* since it does
 Google search.
 
 The main goal of *GlobeBotter Redux* is to see whether it's practical to
-write BDD tests to evaluate an LLM application's ability to retrieve information from RAG sources.
+write BDD tests that evaluate a RAG application's ability to retrieve information.
 """
 st.markdown(message)
 
