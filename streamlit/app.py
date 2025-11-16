c8241db3ff4ee645b560b13dbef21c43f380d006
Carl Meijer <hammingweight@gmail.com>
2025-11-16 20:01:20 +0200

    Stylistic change.

--- streamlit/app.py
+++ streamlit/app.py
@@ -33,7 +33,7 @@ write BDD tests that evaluate a RAG application's ability to retrieve informatio
 st.markdown(message)
 
 if "chat_history" not in st.session_state:
-    st.session_state["chat_history"] = []
+    st.session_state.chat_history = []
 
 for message in st.session_state.chat_history:
     with st.chat_message(message["role"]):
